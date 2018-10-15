cea9d2fe89ca327734e63ddace9a438e52e95bc2
leonardo Kaplan <kaplanleonardo@gmail.com>
2018-10-15 18:55:47 -0500

    finished bubble sort with yield block

--- bubble.rb
+++ bubble.rb
@@ -23,3 +23,34 @@ def bubble_sort(arr)
 end
 
 bubble_sort(number_arr)
+
+def bubble_sort_by(arr) 
+ final_arr = []
+ big_counter = 0
+ while big_counter < arr.length
+  x = 0
+  y = 1
+  counter = 0
+    while counter < arr.length-1 
+        if yield(arr[x],arr[y]) > 0
+          holder = []
+          holder << arr[x]
+          arr[x] = arr[y]
+          arr[y]= holder[0]
+          
+        end
+      
+      x += 1
+      y += 1
+      counter += 1   
+    end 
+  big_counter += 1
+  end 
+  p arr
+end
+
+array = ['hill','he','hug']
+
+bubble_sort_by(array) do |left,right|
+   left.length - right.length
+ end
